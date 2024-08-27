6c42038c2b5763e5b50cd6b8491ac66de07434bb
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-08-27 11:53:15 +0100

    Task 4 Completed

---
+++ models/articles.models.js
@@ -0,0 +1,8 @@
+const db = require('../db/connection')
+
+exports.selectArticlesById = (id) => {
+    return db.query('SELECT * FROM articles WHERE article_id = $1', [id]).then((data) => {
+        if (data.rows.length === 0) return Promise.reject({status: 404, msg: 'Not Found'})
+        return data.rows
+    })
+}
