e8d5f5e628d25508208d760a00bb0b71296af70b
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-08-28 21:39:24 +0100

    Task 8 Completed

--- app.js
+++ app.js
@@ -1,40 +1,48 @@
-const express = require('express')
-const app = express()
-const { getTopics } = require('./controllers/topics.controllers.js')
-const { getApi } = require('./controllers/api.controllers.js')
-const { getArticlesById, getArticles, getArticleComments, postArticleComments } = require('./controllers/articles.controllers.js')
+const express = require("express");
+const app = express();
+const { getTopics } = require("./controllers/topics.controllers.js");
+const { getApi } = require("./controllers/api.controllers.js");
+const {
+  getArticlesById,
+  getArticles,
+  getArticleComments,
+  postArticleComments,
+  patchArticle,
+} = require("./controllers/articles.controllers.js");
 
 app.use(express.json());
 
-app.get('/api/topics', getTopics)
-app.get('/api', getApi)
-app.get('/api/articles/:article_id', getArticlesById)
-app.get('/api/articles', getArticles)
-app.get('/api/articles/:article_id/comments', getArticleComments)
+app.get("/api/topics", getTopics);
+app.get("/api", getApi);
+app.get("/api/articles/:article_id", getArticlesById);
+app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id/comments", getArticleComments);
 
-app.post('/api/articles/:article_id/comments', postArticleComments)
+app.post("/api/articles/:article_id/comments", postArticleComments);
 
-app.use((err, req, res, next) => {
-    if(err.status && err.msg){
-        res.status(err.status).send({msg: err.msg})
-    }else{
-        next(err)
-    }
-})
-app.use((err, req, res, next) => {
-    if(err.code === '22P02'){
-        res.status(400).send({msg: 'Bad Request'})
-    }else{
-        next(err)
-    }
-})
+app.patch("/api/articles/:article_id", patchArticle);
 
 app.use((err, req, res, next) => {
-    if(err.code === '23503'){
-        res.status(404).send({msg: 'Article Not Found'})
-    }else{
-        next(err)
-    }
-})
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    next(err);
+  }
+});
 
-module.exports = app
+app.use((err, req, res, next) => {
+  if (err.code === "23503") {
+    res.status(404).send({ msg: "Article Not Found" });
+  } else {
+    next(err);
+  }
+});
+
+module.exports = app;
--- models/articles.models.js
+++ models/articles.models.js
@@ -1,27 +1,59 @@
-const db = require('../db/connection')
+const db = require("../db/connection");
 
 exports.selectArticlesById = (id) => {
-    return db.query('SELECT * FROM articles WHERE article_id = $1', [id]).then((data) => {
-        if (data.rows.length === 0) return Promise.reject({status: 404, msg: 'Not Found'})
-        return data.rows
-    })
-}
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1", [id])
+    .then((data) => {
+      if (data.rows.length === 0)
+        return Promise.reject({ status: 404, msg: "Not Found" });
+      return data.rows;
+    });
+};
 exports.selectArticles = () => {
-    return db.query('SELECT article_id, title, topic, author, created_at, votes, article_img_url FROM articles ORDER BY created_at DESC').then((data) =>{
-        return data.rows
-    })
-}
+  return db
+    .query(
+      "SELECT article_id, title, topic, author, created_at, votes, article_img_url FROM articles ORDER BY created_at DESC"
+    )
+    .then((data) => {
+      return data.rows;
+    });
+};
 
 exports.selectArticleComments = (id) => {
-    return db.query('SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC', [id]).then(({rows}) => {
-        if (!(rows.length)) return Promise.reject({status: 404, msg: 'Not Found'}) 
-        return rows
-    })
-}
+  return db
+    .query(
+      "SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC",
+      [id]
+    )
+    .then(({ rows }) => {
+      if (!rows.length)
+        return Promise.reject({ status: 404, msg: "Not Found" });
+      return rows;
+    });
+};
 
 exports.insertArticleComments = (username, body, id) => {
-    const insertVal = [username, body, id]
-    return db.query('INSERT INTO comments (author, body, article_id) VALUES ($1, $2, $3) RETURNING *', insertVal).then((data) => {
-        return data.rows[0]
-    })
-}
+  const insertVal = [username, body, id];
+  return db
+    .query(
+      "INSERT INTO comments (author, body, article_id) VALUES ($1, $2, $3) RETURNING *",
+      insertVal
+    )
+    .then((data) => {
+      return data.rows[0];
+    });
+};
+
+exports.updateArticle = (inc_votes, id) => {
+  const insertVal = [inc_votes, id];
+  return db
+    .query(
+      "UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *",
+      insertVal
+    )
+    .then(({ rows }) => {
+      if (!rows.length)
+        return Promise.reject({ status: 404, msg: "Not Found" });
+      return rows[0];
+    });
+};
