820e46bdfb296379163b60b7ee0ef8c02699d734
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-09-05 22:37:34 +0100

    Hosting Completed

--- db/connection.js
+++ db/connection.js
@@ -3,18 +3,17 @@ const ENV = process.env.NODE_ENV || "development";
 
 const config = {};
 
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
-module.exports = new Pool(config);
-
-require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
